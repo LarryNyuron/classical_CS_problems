6abfd2ee2d5c33dcbe90b8857d1feb9377076e2d
LarryNyuron <mikhkravcoff@gmail.com>
2023-03-22 08:19:34 +0300

    Add binar search, README.md with TODO ex.

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO:
+
+home work 1 []
+searchDNA:
+- for binary search use the module bisect
--- searchDNA.py
+++ searchDNA.py
@@ -6,10 +6,10 @@ Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
 Gene = List[Codon]
 
-gene_str = 'ACGТGGCTCTCТAACGTACGТACGТACGGGGTПATATATACCCTAGGACТCCCПT'
+gene_str = 'ATTACACACATTGGGA'
 
 
-def string_to_gene(s: str) -> Gene:
+def stringToGene(s: str) -> Gene:
     gene: Gene = []
     for i in range(0, len(s), 3):
         if (i + 2) >= len(s):
@@ -19,3 +19,28 @@ def string_to_gene(s: str) -> Gene:
     return gene
 
 my_gene: Gene = string_to_gene(gene_str)
+
+print(my_gene)
+
+
+#linear search O(n)
+def linearSearch(gene: Gene, k_codon: Codon) -> bool:
+    for i in Gene:
+        if i == k_codon:
+            return True
+    return False
+
+
+#bi search O(lg(n)), but O(n*lg(n)) if first we sorting list
+def binSearch(gene: Gene, k_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high:
+        mid: int = (low + high) // 2
+        if gene[mid] < k_codon:
+            low = mid + 1
+        elif gene[mid] > k_codon:
+            high = mid - 1
+        else:
+            return True
+    return False
