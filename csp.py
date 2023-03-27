5e689f4c7efc71236ebaa26150ef6c5af7634045
LarryNyuron <mikhkravcoff@gmail.com>
2023-03-27 19:14:04 +0300

    Add exer-s Map Coloring and Queens

--- csp.py
+++ csp.py
@@ -12,6 +12,7 @@ class Constraint(Generic[V,D], ABC):
 
     @abstractmethod
     def satisfied(self, assignment: Dict[V,D]) -> bool:
+        pass
 
 
 class CSP(Generic[V,D]):
@@ -32,3 +33,31 @@ class CSP(Generic[V,D]):
                 raise LookupError('Var in constraint not in CSP')
             else:
                 self.constarints[variable].append(constraint)
+
+    def consistent(self, variable: V, assignment: Dict[V,D]) -> bool:
+        for constraint in self.constarints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
+
+    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+        # assignment is completed if assignment exists
+        # for each variable (base case)
+        if len(assignment) == len(self.variables):
+            return assignment
+
+        # give all var from CSP, but not from assignment
+        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+
+        # get all possible values of the definition area
+        # for the first variable without assignment
+        first: V = unassigned[0]
+        for value in self.domains[first]:
+            local_assign = assignment.copy()
+            local_assign[first] = value
+            # if there are no contradictions, then we continue the recursion
+            if self.consistent(first, local_assign):
+                result: Optional[Dict[V, D]] = self.backtracking_search(local_assign)
+                if result is not None:
+                    return result
+        return None
