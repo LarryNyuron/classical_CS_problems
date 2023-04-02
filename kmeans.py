706a2eb257ba420c39a77b4a0fa1100fc07dce62
LarryNyuron <mikhkravcoff@gmail.com>
2023-04-02 19:25:54 +0300

    kmeans

--- kmeans.py
+++ kmeans.py
@@ -14,3 +14,28 @@ def zscores(original: Sequence[float]) -> List[float]:
     if std == 0:
         return [0] * len(original)
     return [(x - avg) / std for x in original]
+
+
+Point = TypeVar('Point', bound=DataPoint)
+
+
+class KMeans(Generic[Point]):
+    @dataclass
+    class Cluster:
+        points: List[Point]
+        centroid: DataPoint
+
+    def __init__(self, k: int, points: List[Point]) -> None:
+        if k < 1:
+            raise ValueError('k must be >= 1')
+        self._points: List[Point] = points
+        self._zscore_normalize()
+        self._clusters: List[KMeans.Cluster] = []
+        for _ in range(k):
+            rand_point: DataPoint = self._random_point()
+            cluster: KMeans.Cluster = KMeans.Cluster([], rand_point)
+            self._clusters.append(cluster)
+
+    @property
+    def _centroids(self) -> List[DataPoint]:
+        return [x.centroid for x in self._clusters]
